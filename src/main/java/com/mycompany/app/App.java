ae8a25fb6cd4531fc1b05f5028ed54389b679923
Abhishek Dhingra <54358904+theabhishekdhingra@users.noreply.github.com>
2024-05-01 16:53:28 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,6 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-        String password = "abcdef";
     }
 
     private final String getMessage() {
